daba192b15ec3777696ef97b264b775a5225b3da
Shadi A <shadi.abdelfatah@gmail.com>
2017-02-21 21:03:56 +0100

    switched seleniumReader to headless mode

--- seleniumReader.py
+++ seleniumReader.py
@@ -1,10 +1,15 @@
-import time  
+import time
 from selenium import webdriver
+from pyvirtualdisplay import Display
+
 
 def getHtml(url):
+    display = Display(visible=0, size=(800, 600))
+    display.start()
     browser = webdriver.Firefox()
     ffResults = browser.get(url)
-    time.sleep(5)
-    full_content = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")  
+    time.sleep(30)
+    full_content = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
     browser.quit()
+    display.stop()
     return full_content
