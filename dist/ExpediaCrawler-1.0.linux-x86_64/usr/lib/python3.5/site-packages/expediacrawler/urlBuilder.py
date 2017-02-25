c0edf25148a55951aeaad69aa5332ec4b554d71a
Shadi A <shadi.abdelfatah@gmail.com>
2017-02-25 19:14:42 +0100

    added build and dist to gitignore

--- dist/ExpediaCrawler-1.0.linux-x86_64/usr/lib/python3.5/site-packages/expediacrawler/urlBuilder.py
+++
@@ -1,5 +0,0 @@
-def build_url(origin_city, destination_city, start_date, return_date):
-    return "https://www.expedia.com/Flights-Search?trip=roundtrip&leg1=from:{org_city},to:{dest_city}," \
-           "departure:{origin_date}TANYT&" \
-           "leg2=from:{dest_city},to:{org_city},departure:{return_date}TTANYT&passengers=adults:1&mode=search" \
-        .format(org_city=origin_city, dest_city=destination_city, origin_date=start_date, return_date=return_date)
