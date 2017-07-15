97864e1547e5a22526868d28c8302971bfca62df
Shadi A <shadi.abdelfatah@gmail.com>
2017-07-15 21:08:49 +0200

    added csv file writer function

--- expediacrawler/offersWriter.py
+++ expediacrawler/offersWriter.py
@@ -1,5 +1,18 @@
+import csv
+
+
 def print_offers_stdout(all_offers):
     for day in all_offers:
         print(day, ":")
         for offer in all_offers[day]:
             print(offer)
+
+
+def append_to_csv(all_offers, file_name):
+    with open(file_name, 'a') as f:
+        writer = csv.writer(f)
+        for day in all_offers:
+            for offer in all_offers[day]:
+                offer_info = list(offer.values());
+                offer_info.append(day)
+                writer.writerow(offer_info)
